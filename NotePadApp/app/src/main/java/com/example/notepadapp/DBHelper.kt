67261196dbd35c5e144d2e593065e9d84f019ea1
Jbrochan <hyeongchanj@gmail.com>
2023-07-14 16:18:03 +0900

    refactor: SQLite 데이터 베이스 수정 - 데이터 베이스 내부 UserTable 삭제 - UserDAO 삭제

--- NotePadApp/app/src/main/java/com/example/notepadapp/DBHelper.kt
+++ NotePadApp/app/src/main/java/com/example/notepadapp/DBHelper.kt
@@ -6,17 +6,11 @@ import android.database.sqlite.SQLiteOpenHelper
 
 class DBHelper(context: Context) : SQLiteOpenHelper(context, "note_pad_database.db", null, 1) {
     override fun onCreate(sqliteDatabase: SQLiteDatabase?) {
-        val queryUser = """create table UserTable(
-            |idx integer primary key autoincrement,
-            |userPassword integer not null)
-        """.trimMargin()
-
         val queryCategory = """create table if not exists CategoryTable(
             |idx integer primary key autoincrement,
             |categoryTitle text not null)
         """.trimMargin()
 
-        sqliteDatabase?.execSQL(queryUser)
         sqliteDatabase?.execSQL(queryCategory)
     }
 
